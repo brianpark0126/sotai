590e28e036ee7050d5b3da9957d11211cd3b9775
Linus Meyer-Teruel <linus@Linuss-MacBook-Pro.local>
2023-07-27 12:04:00 -0700

    chore: fixing camelcase in inference

--- tests/test_api.py
+++ tests/test_api.py
@@ -206,7 +206,7 @@ def test_post_trained_model(
 @patch("builtins.open")
 @patch(
     "requests.post",
-    return_value=MockResponse({"inferenceConfigUUID": "test_inference_uuid"}),
+    return_value=MockResponse({"inference_config_uuid": "test_inference_uuid"}),
 )
 @patch("sotai.api.get_api_key", return_value="test_api_key")
 def test_post_inferencel(
