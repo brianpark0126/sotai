2b82b26fa7e7b9759125056868f9fe0450c01cde
William Bakst <will@sotai.ai>
2023-09-07 14:41:53 -0700

    fix: add tqdm to pyproject dependency list to resolve import error on fresh environment installation of the sotai package.

--- sotai/__init__.py
+++ sotai/__init__.py
@@ -3,7 +3,7 @@
 # This version must always be one version ahead of the current release, so it
 # matches the current state of development, which will always be ahead of the
 # current release. Use Semantic Versioning.
-__version__ = "0.6.0"
+__version__ = "0.6.1"
 
 from . import api, constants, data, demo, external, features, layers, models, training
 from .enums import *
